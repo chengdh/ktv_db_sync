ddd9cda83b467c5988f111d2c48c5827f746084c
chengdh <cheng.donghui@gmail.com>
2013-11-08 11:16:52 +0800

    change csv header display

--- export_product_uom.sh
+++ export_product_uom.sh
@@ -8,14 +8,13 @@ $sqsh -DeVideoBill_SH -S192.168.8.252 -Usa -Pvonger -mbcp -o$out_fl <<QRY
 use eVideoBill_SH
 \set bcp_colsep=","
 \set bcp_rowsep=""
-SELECT 'id','name','category_id:id','factor'
-  UNION
+\echo id,name,category_id:id,factor
 SELECT DISTINCT 
      a.MaterialUnitCode AS id,
      a.MaterialUnit AS name,
      'product.product_uom_categ_unit' AS 'category_id:id',
      '1' AS factor
 FROM BL_TBL_MaterialCode a;
-\go
+\go -f
 quit
 QRY
