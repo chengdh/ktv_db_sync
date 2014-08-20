105cc89ab97013b7748427700dc5ec962b7e9ffd
chengdh <cheng.donghui@gmail.com>
2014-08-20 16:07:49 +0800

    change imp2mssql.sh

--- imp2mssql.sh
+++ imp2mssql.sh
@@ -8,4 +8,27 @@ mssql_host=192.168.8.252
 
 sqsh=/usr/bin/sqsh
 
-$sqsh -D $mssql_db -S $mssql_host -U $mssql_user -P $mssql_pw -mbcp -i /home/openerp/ktv_db_sync/test_import2db.sqsh
+$sqsh -D $mssql_db -S $mssql_host -U $mssql_user -P $mssql_pw -mbcp <<SQL
+use eVideoBill_SH
+\GO
+-- BL_TBL_Material_Sort
+DROP TABLE TMP_BL_TBL_Material_Sort
+\GO
+CREATE TABLE TMP_BL_TBL_Material_Sort
+(
+  MaterialSortID INT,
+  MaterialSortName	VARCHAR(100),
+  HeadMaterialSortID INT
+)
+\GO
+BULK
+INSERT TMP_BL_TBL_Material_Sort
+FROM '.\export\BL_TBL_Material_Sort.csv'
+WITH
+(
+  FIELDTERMINATOR = ',',
+  ROWTERMINATOR = '\n'
+)
+\GO -f
+quit
+SQL
