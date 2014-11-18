c7f10955fba16564fdf1d75dd2027b10c70e50ed
chengdh <cheng.donghui@gmail.com>
2014-11-18 12:39:22 +0800

    添加了判断是否需要同步处理的条件

--- exp_mssql_imp_openerp.sh
+++ exp_mssql_imp_openerp.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#判断是否需要执行导入操作
+./get_mssql_state.sh
+mssql_state=`cat export/mssql_state.csv`
 #先执行从mssql中导出数据,然后再导入到openerp
-./mssql_export_from_mssql.sh
-./imp2openerp
+if [$mssql_state == 0.00]
+  then
+    ./mssql_export_from_mssql.sh
+    ./imp2openerp
+fi
