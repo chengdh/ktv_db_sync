67b3263b40fa3735041bb10501cf2a0780685933
chengdh <cheng.donghui@gmail.com>
2014-11-18 15:45:49 +0800

    change import2mssql code

--- exec_export.sh
+++ exec_export.sh
@@ -14,11 +14,14 @@ postgres_db=test_sync
 #postgres_db=ktv_dev
 
 
-for file in ` ls ${work_dir}`
-do
-  if [ -f ${file} ] && [ ${file:0:6} = "export" ]
-  then
-    echo "导出文件:"${file}
-    PGPASSWORD=$postgres_pwd psql -h${postgres_server} -d${postgres_db} -p${postgres_port} -U${postgres_user} -f${file}
-  fi
-done
+
+#for file in ` ls ${work_dir}`
+#do
+#  if [ -f ${file} ] && [ ${file:0:6} = "export" ]
+#  then
+#    echo "导出文件:"${file}
+#    PGPASSWORD=$postgres_pwd psql -h${postgres_server} -d${postgres_db} -p${postgres_port} -U${postgres_user} -f${file}
+#  fi
+#done
+echo "导出文件: BL_TBL_Material_Storage"
+PGPASSWORD=$postgres_pwd psql -h${postgres_server} -d${postgres_db} -p${postgres_port} -U${postgres_user} -f export_BL_TBL_Material_Storage
