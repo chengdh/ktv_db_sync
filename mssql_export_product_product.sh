2b750ca1aaecb179aa582c5af7d064b91c93816f
chengdh <cheng.donghui@gmail.com>
2014-11-18 15:23:57 +0800

    add external_id to product.product

--- mssql_export_product_product.sh
+++ mssql_export_product_product.sh
@@ -9,7 +9,7 @@ $sqsh -D$mssql_db -S$mssql_host -U$mssql_user -P$mssql_pw -mbcp -o$out_fl <<QRY
 use eVideoBill_SH
 \set bcp_colsep=","
 \set bcp_rowsep=""
-\echo id,name,code,uom_id:id,uom_po_id:id,type,categ_id:id,standard_price,list_price,description
+\echo id,name,code,uom_id:id,uom_po_id:id,type,categ_id:id,standard_price,list_price,description,external_id
 SELECT 
 'ktv_product_product_' + CAST(b.WineMaterialID AS VARCHAR(20))  AS id,
 b.MaterName AS name,
@@ -20,7 +20,8 @@ b.MaterialBarcode AS code,
 'ktv_product_category_' + CAST(b.MaterialSortID AS VARCHAR(20)) AS  'categ_id:id',
 b.PriceA AS standard_price,
 b.PriceB AS list_price,
-b.MaterialCode AS description
+b.MaterialCode AS description,
+b.id
 FROM BL_TBL_MaterialCode b
 -- WHERE SellSign = 1 AND IsVisible = 1;
 \go -f
