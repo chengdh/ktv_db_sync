4490865ebd99ab54beaad62a3d662d7e28b3dc0d
chengdh <cheng.donghui@gmail.com>
2014-11-18 22:44:35 +0800

    add exec workflow code

--- exp_openerp_imp_mssql.sh
+++ exp_openerp_imp_mssql.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-#先执行从openerp中导出数据,然后再导入到mssql
-./exec_export.sh
-./imp2mssql.sh
+#判断是否需要执行导入操作
+./get_openerp_state.sh
+openerp_state=`cat export/openerp_state.csv`
+#先执行从mssql中导出数据,然后再导入到openerp
+if [ $openerp_state = 1 ]
+  then
+    echo "开始同步..."
+    ./exec_export.sh
+    ./imp2mssql.sh
+    echo "同步结束"
+fi
