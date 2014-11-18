e533578b56bbfa324b2e6950384d2a8b0e5aa00d
chengdh <cheng.donghui@gmail.com>
2014-11-18 11:49:19 +0800

    add sh to get mssql state

---
+++ get_mssql_state.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "判断mssql的状态,是否需要同步"
+out_fl=export/mssql_state.csv
+rm -f $out_fl
+sqsh=/usr/bin/sqsh
+rm -f $out_fl
+$sqsh -D$mssql_db -S$mssql_host -U$mssql_user -P$mssql_pw -mbcp -o$out_fl <<QRY
+use eVideoBill_SH
+\set bcp_colsep=","
+\set bcp_rowsep=""
+
+SELECT sum(b.StorageNumber)   FROM BL_TBL_MaterialCode a,BL_TBL_Material_Storage b
+WHERE a.WineMaterialID  = b.WineMaterialID  
+AND a.MaterName='ERP'
+AND b.StorageUnitID = 1
+\go -f
+quit
+QRY
+
