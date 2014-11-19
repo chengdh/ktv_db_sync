c568e619487c0b0e0af991935bc77c4656dbe913
chengdh <cheng.donghui@gmail.com>
2014-11-19 21:53:55 +0800

    refactor code

--- exp_mssql_imp_openerp.sh
+++ exp_mssql_imp_openerp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-work_dir=$(dirname $0)
-mssql_db=eVideoBill_SH
-mssql_user=sa
-mssql_pw=vonger
-mssql_host=192.168.8.252
+export work_dir=$(dirname $0)
+export mssql_db=eVideoBill_SH
+export mssql_user=sa
+export mssql_pw=vonger
+export mssql_host=192.168.8.252
 
 #判断是否需要执行导入操作
 ./get_mssql_state.sh
-mssql_state=`cat export/mssql_state.csv`
+export mssql_state=`cat export/mssql_state.csv`
 #先执行从mssql中导出数据,然后再导入到openerp
 if [ $mssql_state = 0.00 ]
   then
